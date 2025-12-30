783d8d7ae53d42ece6e6a9380232d384359f7ac3
SupAdri <adrian2005hc@gmail.com>
2025-12-29 21:49:08 -0500

    a ver si ahora pincha

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ var User = [{
 
 //para enviar el feed de quotes
 app.get('/api/feed', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.send(getFeed());
 });
 
